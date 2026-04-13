373f19e762a65e785c9a6ab0a3204e4a0a28a8dc
Mariana <marianalopes697@gmail.com>
2026-04-13 12:55:56 -0300

    feat: estilização form login

--- src/app/components/form-login/form-login.ts
+++ src/app/components/form-login/form-login.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { ButtonModule } from 'primeng/button'
+import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+
+
+const COMPONENTS = [ButtonModule, CardModule, FormsModule, FloatLabelModule, InputTextModule]
+
 
 @Component({
   selector: 'app-form-login',
-  imports: [ButtonModule],
+  imports: [...COMPONENTS],
   templateUrl: './form-login.html',
   styleUrl: './form-login.css',
 })
