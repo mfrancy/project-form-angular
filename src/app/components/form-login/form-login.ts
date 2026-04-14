60eeb0c76f863d4e6242da31231be2a6fb960300
Mariana <marianalopes697@gmail.com>
2026-04-13 23:01:02 -0300

    feat: estruturando formulário

--- src/app/components/form-login/form-login.ts
+++ src/app/components/form-login/form-login.ts
@@ -1,21 +1,43 @@
-import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button'
+import { Component, inject, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
 
+const COMPONENTS = [
+  ButtonModule,
+  CardModule,
+  FormsModule,
+  FloatLabelModule,
+  InputTextModule,
+  PasswordModule,
+];
 
-
-const COMPONENTS = [ButtonModule, CardModule, FormsModule, FloatLabelModule, InputTextModule]
-
+const MODULES = [
+  ReactiveFormsModule
+]
 
 @Component({
   selector: 'app-form-login',
-  imports: [...COMPONENTS],
+  imports: [...COMPONENTS, ...MODULES],
   templateUrl: './form-login.html',
   styleUrl: './form-login.css',
 })
-export class FormLogin {
+export class FormLogin implements OnInit {
+  fb = inject(FormBuilder);
+  formSubscribe: any;
 
+  onSubmit() {}
+
+  ngOnInit() {
+    this.formSubscribe = this.fb.group({                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+      name: ['OI', Validators.required],
+      nascimento: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
 }
